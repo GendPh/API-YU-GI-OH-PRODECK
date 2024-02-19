3a0b480403ceb30302a4e8a0a186b0e7da40b6a7
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-02-19 20:34:59 +0000

    save

--- Card/LoadAllCards.js
+++ Card/LoadAllCards.js
@@ -1,26 +1,40 @@
-import { FetchApi, GetValueFromURL, shuffleArray, paginateObject } from "../src/API/FetchApi.js";
-import { CardElement, LoadCards } from "../src/CreateCards/CreateCard.js";
+// Importing necessary modules
+import { FetchApi, GetValueFromURL, paginateObject } from "../src/API/FetchApi.js";
+import { LoadCards } from "../src/CreateCards/CreateCard.js";
 
-//Card ID: "https://db.ygoprodeck.com/api/v7/cardinfo.php?id=1861630";
-
+// Function to load all cards based on URL parameters
 async function LoadAllCards() {
+  // Get page number, archetype, type, and race from URL
   const page_number = GetValueFromURL("page");
   const archetype = GetValueFromURL("archetype");
   const archetype_value = (!archetype.error) ? `archetype=${archetype}&` : "";
   const type = GetValueFromURL("type");
   const type_value = (!type.error) ? `type=${type}&` : "";
-  const URL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?${archetype_value}${type_value}`;
+  const race = GetValueFromURL("race");
+  const race_value = (!race.error) ? `race=${race}&` : "";
+  
+  // Construct URL based on parameters
+  const URL = `https://db.ygoprodeck.com/api/v7/cardinfo.php?${archetype_value}${type_value}${race_value}`;
+  
+  // Get the container for all cards
   const card_container = document.getElementById("all-cards");
   console.log(URL);
+  
+  // Fetch data from API
   let data = await FetchApi(URL);
+  
+  // If no error, paginate data and load buttons
   if (!data.error) {
     const paged_data = paginateObject(data.data, 50);
     data = (page_number <= paged_data.length) ? paged_data[page_number - 1] : { error: "No cards available" };
     LoadButtons(paged_data, page_number);
   }
+  
+  // Load cards into container
   LoadCards(card_container, data);
 }
 
+// Function to load pagination buttons
 function LoadButtons(data, page) {
   const button_container = document.getElementById("page-buttons");
   for (let i = 0; i < data.length; i++) {
@@ -30,6 +44,8 @@ function LoadButtons(data, page) {
     }
     button_container.appendChild(link);
   }
+  
+  // Scroll to center the active button
   const targetElement = button_container.querySelector('.active');
   if (targetElement) {
     const containerWidth = button_container.clientWidth; // Width of the container
@@ -43,9 +59,9 @@ function LoadButtons(data, page) {
       left: scrollPosition,
     });
   }
-
 }
 
+// Function to create pagination buttons
 function CreateButtons(page) {
   const a = document.createElement("a");
   a.href = `./allCards.html?page=${page}`;
@@ -54,100 +70,5 @@ function CreateButtons(page) {
   return a;
 }
 
-const searchForm = document.getElementById('searchForm');
-
-// Function to handle form submission
-async function handleSubmit(event) {
-  event.preventDefault();
-  const container = document.getElementById("card-search");
-  const html = `
-  <!-- Error Message state -->
-  <p class="error-message hidden">We're sorry, but we encountered an issue while trying to retrieve the requested data. Please try again later. If the problem persists, feel free to contact our support team for assistance.
-  </p>
-  <!-- Loader for loading state -->
-  <div class="loader-container col-span-3 md:col-span-5 lg:col-span-10">
-    <span class="loader"></span>
-  </div>`;
-
-  container.innerHTML = html;
-
-  const searchTerm = document.getElementById('searchInput').value.trim();
-  if (searchTerm !== '') {
-    let URL = "https://db.ygoprodeck.com/api/v7/cardinfo.php";
-    let card_result = await FetchApi(URL + `?fname=${searchTerm}`);
-    container.classList.replace("hidden", "grid");
-    card_result = (!card_result.error) ? card_result.data.slice(0, 20) : { error: "No cards collected! " };
-    LoadCards(container, card_result);
-  } else {
-    container.classList.replace("grid", "hidden");
-  }
-}
-
-async function LoadSelectOptionsArchetype(url) {
-  const archetypes = await FetchApi(url);
-  const archetype = GetValueFromURL("archetype");
-  const selectContainer = document.getElementById("archetype_select");
-  archetypes.forEach(arch => {
-    const option = document.createElement("option");
-    option.value = arch.archetype_name;
-    option.textContent = arch.archetype_name;
-    if (archetype == arch.archetype_name) {
-      option.selected = true;
-    }
-    selectContainer.appendChild(option);
-  });
-}
-
-function LoadSelectOptionsType() {
-  const type_array = ["Effect Monster", "Flip Effect Monster", "Flip Tuner Effect Monster", "Gemini Monster", "Normal Monster", "Normal Tuner Monster", "Pendulum Effect Monster", "Pendulum Effect Ritual Monster", "Pendulum Flip Effect Monster", "Pendulum Normal Monster", "Pendulum Tuner Effect Monster", "Ritual Effect Monster", "Ritual Monster", "Spell Card", "Spirit Monster", "Toon Monster", "Trap Card", "Tuner Monster", "Union Effect Monster", "Fusion Monster", "Link Monster", "Pendulum Effect Fusion Monster", "Synchro Monster", "Synchro Pendulum Effect Monster", "Synchro Tuner Monster", "XYZ Monster", "XYZ Pendulum Effect Monster", "Skill Card", "Token"];
-  const type = GetValueFromURL("type");
-  const selectContainer = document.getElementById("type_select");
-  type_array.forEach(t => {
-    const option = document.createElement("option");
-    option.value = t;
-    option.textContent = t;
-    if (type == t) {
-      option.selected = true;
-    }
-    selectContainer.appendChild(option);
-  });
-}
-
-function HandleSelectedChange(container, id) {
-  // Get the selected value of the select element
-  const selectedValue = container.value;
-
-  // Update the URL with the selected value
-  const url = new URL(window.location.href);
-
-  // Set the page parameter to 1
-  url.searchParams.set('page', '1');
-
-  // If the selected value is not empty, set the parameter in the URL
-  if (selectedValue.trim() !== '') {
-    url.searchParams.set(id, selectedValue);
-  } else {
-    // If the selected value is empty, remove the parameter from the URL
-    url.searchParams.delete(id);
-  }
-
-  // Reload the page with the updated URL
-  window.location.href = url.toString();
-}
-
-
-const archetypeSelectContainer = document.getElementById("archetype_select");
-const typeSelectContainer = document.getElementById("type_select");
-
-archetypeSelectContainer.addEventListener("change", () => { HandleSelectedChange(archetypeSelectContainer, "archetype") });
-
-typeSelectContainer.addEventListener("change", () => { HandleSelectedChange(typeSelectContainer, "type") });
-
-
-
-LoadSelectOptionsArchetype("https://db.ygoprodeck.com/api/v7/archetypes.php")
-LoadSelectOptionsType();
-// Add event listener for form submission
-// searchForm.addEventListener('submit', handleSubmit);
-// searchForm.addEventListener('input', handleSubmit);
+// Load all cards when the page loads
 LoadAllCards();
